d8d216ef8e9d76d45cfb209c5637b24a9496131b
BeastnodeG <107003945+BeastnodeG@users.noreply.github.com>
2022-06-10 21:45:40 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# toonvalues.github.io
+The toonvalues Quiz measures individuals' toontown values.
---
+++ questions.js
@@ -0,0 +1,447 @@
+questions = [
+    {
+		//increasing econ makes simple higher
+		//increasing dipl makes leader higher
+		//increasing govt makes quanity higher
+		//increasing scty makes progress higher
+        "question": "Spamming Sound is lame.",
+        "effect": {
+            "econ": -5,
+            "dipl": 1,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I frequently find myself directing the team.",
+        "effect": {
+            "econ": 0,
+            "dipl": 15,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Unprestige Zap is a waste of a track.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Prestiged gag tracks make the game too complicated.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Zap is confusing.",
+        "effect": {
+            "econ": 5,
+            "dipl": -1,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+    {
+        "question": "Toon up is a gag track that must be prestiged.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -3,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Throw currently has a useless prestige.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 1,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I like the new Lawbot designs.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+	{
+        "question": "1 laff ubers should still be supported.",
+        "effect": {
+            "econ": -2,
+            "dipl": 2,
+            "govt": 0,
+            "scty": -2
+        }
+    },
+	{
+        "question": "I avoid grouping with 1 laff ubers.",
+        "effect": {
+            "econ": 0,
+            "dipl": -5,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+	{
+        "question": "Squirt is a gag that must be prestiged, outside of specific circumstances.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+	{
+        "question": "I frequently use syphon strategies, when viable.",
+        "effect": {
+            "econ": -5,
+            "dipl": 2,
+            "govt": 0,
+            "scty": 2
+        }
+    },
+	{
+        "question": "I frequently use double zap strategies, when viable.",
+        "effect": {
+            "econ": -5,
+            "dipl": 2,
+            "govt": 0,
+            "scty": 2
+        }
+    },
+	{
+        "question": "I have made an effort towards learning zap.",
+        "effect": {
+            "econ": -1,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 2
+        }
+    },
+	{
+        "question": "The OCLO has an appropriate difficulty.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 3
+        }
+    },
+	{
+        "question": "I can effectively pull off zap combos when possible.",
+        "effect": {
+            "econ": -5,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+	{
+        "question": "SOS cards are fine in their current state.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+	{
+        "question": "I avoid grouping with soundless toons. Especially in facility groups.",
+        "effect": {
+            "econ": 0,
+            "dipl": -5,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+	{
+        "question": "Toontown Corporate clash is too easy.",
+        "effect": {
+            "econ": -3,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 3
+        }
+    },
+	{
+        "question": "Attack and Defense specialist cogs make the game more interesting.",
+        "effect": {
+            "econ": -1,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+	{
+        "question": "Storm cloud should deal 1 more point of damage.",
+        "effect": {
+            "econ": -6,
+            "dipl": 0,
+            "govt": -1,
+            "scty": 0
+        }
+    },
+	{
+        "question": "When sound is no longer an option I usually go for trap.",
+        "effect": {
+            "econ": 5,
+            "dipl": -5, 
+            "govt": 0,
+            "scty": 0
+        }
+    },
+	{
+        "question": "The battle timer should be increased to have more time.",
+        "effect": {
+            "econ": -3,
+            "dipl": 3,
+            "govt": 0,
+            "scty": 1
+        }
+    },
+	{
+        "question": "The VP and Sellbot department needs to be revamped.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 1
+        }
+    },
+	{
+        "question": "The CFO and Cashbot department needs to be revamped.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 1
+        }
+    },
+	{
+        "question": "The CEO and Bossbot department needs to be revamped.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 2
+        }
+    },
+	{
+        "question": "The CJ was superior to the CLO(excluding the addition of the OCLO)",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+	{
+        "question": "There are builds that my build just doesn't work at all with.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -3,
+            "scty": 0
+        }
+    },
+	{
+        "question": "Cog Facilities should be more challenging.",
+        "effect": {
+            "econ": 0,
+            "dipl": 3,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+	{
+        "question": "I prefer to four man bosses.",
+        "effect": {
+            "econ": -2,
+            "dipl": 2,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+	{
+        "question": "Craning in the CFO has been made too difficult.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+	{
+        "question": "The CEO is too unfair.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+	{
+        "question": "I will unite for my group.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+	{
+        "question": "I will unite for my group because of my own mistakes.",
+        "effect": {
+            "econ": 0,
+            "dipl": 2,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+	{
+        "question": "ToonHQ is a negative influence on Toontown.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 2
+        }
+    },
+	{
+        "question": "Toontown is too grindy.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 1
+        }
+    },
+	{
+        "question": "People should not unite for me.",
+        "effect": {
+            "econ": 0,
+            "dipl": 5,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+	{
+        "question": "I will gag up all during the OCLO intermission.",
+        "effect": {
+            "econ": 0,
+            "dipl": 4,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+	{
+        "question": "Calculating damage is too complicated.",
+        "effect": {
+            "econ": 5,
+            "dipl": -2,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+	{
+        "question": "Gag barrels in cog facilities make the game far too easy.",
+        "effect": {
+            "econ": -3,
+            "dipl": 3,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+	{
+        "question": "I am excited for the future of Clash over different Toontown servers.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+	{
+        "question": "Clash needs more complicated mechanics.",
+        "effect": {
+            "econ": -3,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+	{
+        "question": "Zap is overrated",
+        "effect": {
+            "econ": 1,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+	{
+        "question": "Lure is a must have prestige.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -2,
+            "scty": 0
+        }
+    },
+	{
+        "question": "Having many prestiges is unessasary.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 6,
+            "scty": 0
+        }
+    },
+	{
+        "question": "More people would play TTCC if they were aware of it.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 5
+        }
+    },
+	{
+        "question": "I strive to reach mastery in Zap, or have mastered it.",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": -1,
+            "scty": 0
+        }
+    },
+	{
+        "question": "Toontown is a game for all ages.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 8
+        }
+    },
+	{
+        "question": "New players are not helping the community.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+];
